b88d24b5a283397e2c3cc431a25c8327eac89d11
aurumdev952 <117060315+Aurumdev952@users.noreply.github.com>
2023-08-09 21:17:35 +0200

    db setup

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { AuthModule } from './auth/auth.module';
+import { MediaModule } from './media/media.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      entities: [],
+      synchronize: true,
+      connectString: process.env.DATABASE_URL,
+    }),
+    AuthModule,
+    MediaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
